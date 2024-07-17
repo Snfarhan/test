d9b786d0dfc5ea7c638b3d5d5d4e8c81f0310127
Snfarhan <116169228+Snfarhan@users.noreply.github.com>
2024-07-17 15:13:29 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
-import pyarrow as pa
-from streamlit_option_menu import option_menu  # Import the correct function or class
+from streamlit_option_menu import option_menu
+import json
+from google.oauth2 import service_account
+from google.cloud import firestore
 
 # Define a function to fetch user data from Firestore
 @st.cache_data
@@ -17,39 +19,46 @@ def get_user_data(username, _db):
     
     return user_data
 
+def get_db():
+    # Authenticate to Firestore with the JSON account key.
+    key_dict = json.loads(st.secrets["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project="vextera-test")
+    return db
+
 # Dashboard page function
 def dashboard_page(db):
-    # Sidebar navigation
     st.sidebar.header(f"Hello, {st.session_state['username']}!")
     with st.sidebar:
-        selected = option_menu(None, ["Dashboard", "Amazon"], 
+        selected = option_menu(None, ["Dashboard"], 
                                icons=['house-door-fill'],
                                orientation="vertical",
                                styles={"container": {"background-color": "#282434"}})
         
-    # Logout button
     if st.sidebar.button("Logout"):
         st.session_state['logged_in'] = False
         st.session_state['username'] = None
-        st.rerun()
+        st.experimental_rerun()
     
-    # Main content based on selected option
     if selected == "Dashboard":
         st.title('Dashboard')
         st.text('Below is the analysis from noon, amazon and shopify platform')
         st.divider()
         
-        st.title(f"Dashboard")
         user_data = get_user_data(st.session_state.username, db)
         
         if user_data:
             df = pd.DataFrame(user_data)
-            st.dataframe(df)
+            st.write(df.dtypes)  # Print data types of the DataFrame columns for debugging
+            st.table(df)
         else:
             st.write("No data available for the user.")
             
         st.divider()
-    
-    elif selected == "Amazon":
-        from amazon import amazon_page  # Import inside function for better control
-        amazon_page()  # Call the function directly
+
+# Initialize Firestore and render the dashboard
+if "logged_in" in st.session_state and st.session_state.logged_in:
+    db = get_db()
+    dashboard_page(db)
+else:
+    st.write("Please log in.")
