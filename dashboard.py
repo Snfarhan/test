dc53d6ae4ab4534f8b17f59de6a2f9167e58eb27
Snfarhan <116169228+Snfarhan@users.noreply.github.com>
2024-07-17 15:58:01 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -59,7 +59,7 @@ def dashboard_page():
       #      st.dataframe(user_data,column_order=['date','ordered_product_sales','units_ordered'])
      #   query2 = duck.sql("select date,ordered_product_sales,units_ordered from user_data ").df()
       #  st.dataframe(query2, use_container_width=True)  
-        st.write("aaaaaahhhh)
+        st.write("aaaaaahhhh")
 
             
         st.divider()
