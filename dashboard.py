9f2a2a25174e7b2205438e1050d06b63992b9bd8
Snfarhan <116169228+Snfarhan@users.noreply.github.com>
2024-07-17 15:05:19 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import pyarrow as pa
 from streamlit_option_menu import option_menu  # Import the correct function or class
 
 # Define a function to fetch user data from Firestore
